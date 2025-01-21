38527f396ae2f04feeaf4f2da4f423596758c472
Sumit D. Dahake <77786015+sumitdahake23@users.noreply.github.com>
2025-01-21 13:06:02 +0530

    Update Navbar.jsx
    Only the spelling is updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <div className=' p-3'>
             <div className='bg-primary container rounded pt-2'>
 
-                <h3 className='p-3 text-center '> CHATBOAT APPLICATION </h3>
+                <h3 className='p-3 text-center '> CHATBOT APPLICATION </h3>
             </div>
         </div>
     )
